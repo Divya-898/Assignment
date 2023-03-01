73567522e4adcbb07d37d33d441d907029e094e1
Divya-898 <tiwaridiv2206@gmail.com>
2023-03-01 20:21:40 +0530

    added media-query

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
 $(document).ready(function(){
-var keyUser = localStorage.getItem("getvalue");
-$("#submit-form").click(function (e) {
-  e.preventDefault();
-  $.ajax({
-    type: "get",
-    url: "login.json",
-    success: function (data) {
-      var obj = { username: $("#username").val(), pass: $("#pass").val() };
-      if (data.username == obj.username && data.password == obj.pass) {
-        localStorage.setItem("getvalue", obj.username);
-        window.location.href = 'index.html';
-      } else {
-        $("#name-error").text("username and password is not matched");
-      }
-    },
+  var keyUser = localStorage.getItem('getvalue');
+  $('#submit-form').click(function (e){
+    e.preventDefault();
+    $.ajax({
+      type: 'get',
+      url: 'login.json',
+      success: function (data) {
+        var obj = { username: $('#username').val(), pass: $('#pass').val() };
+      for(var i=0 ; i<data.length ; i++){ 
+        if (data[i].username == obj.username && data[i].password == obj.pass) {
+          localStorage.setItem('getvalue', obj.username);
+          window.location.href = 'index.html';
+        }  else if (data[i].username != obj.username && data[i].password == obj.pass) {
+          $('#name-error').html('Please enter a valid username and password');
+        } 
+        else if (data[i].username == obj.username && data[i].password != obj.pass) {
+          $('#name-error').html('Please enter a valid password');
+        }
+        else if (obj.username == '' ||  obj.pass == '') {
+          $('#name-error').html('Please enter a username and password');
+        }
+        }}
+    });
   });
-});
 
- $('#pass').keyup(function(){
-  var name = $("#pass").val();
-  var name_regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?])^.{7,9}$";
-  if (name.length == 0) {
-    return $("#name-error").text("password is required");
-  }if (!name.match(name_regex)) {
-   return $("#name-error").text("Please fill valid password");
-  }
-  $("#name-error").text("Password format is valid");
+  $('#pass').keyup(function(){
+    var name = $("#pass").val();
+    var name_regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?])^.{7,9}$";
+    if (!name.match(name_regex)) {
+    return $('#name-error').text('Please fill valid password');
+    }
+    $('#name-error').text('Password format is valid');
+  })
+
+  $('.close, #fa-thumbs-up').click(function(){
+    ("fa-thumbs-up" == $(this).attr("id")) ? $(this).toggleClass('fa-thumbs') : location.reload(); 
+  });
+
+  $('#sign-user').click(function(){
+    keyUser != null ? localStorage.removeItem('getvalue') : 'Login';
+  });
+  keyUser != null ? ($('#sign-user')).html(`<button class="btn btn-primary mt-3">Logout</button>`) &&  $('.profile-name h3, .story-img h3').html(keyUser) : false;
+
+  $('.photo-box .test-popup-link').magnificPopup({
+    type:'image',
+    gallery: {
+      enabled: true
+    }
+  });
+
+  $('#fa-share,#share-img').click(function(e){
+  ('fa-share' == $(this).attr('id')) ? $('.share-img').counterUp() : $('.share-info').counterUp();
+  })
 })
 
-$(".photo-box img, .close, .fa-thumbs-up").click(function () {
-  ("close" == $(this).attr("class")) ? location.reload() : ("fa-thumbs-up" == $(this).attr("data-id")) ? $(".fa-thumbs-up").css("color", "blue") : $(".modal-body").html(`<img src=${$(this).attr("src")} width=470px>`);
-});
 
-$("#sign-user").click(function () {
-  keyUser != null ? localStorage.removeItem("getvalue") : "Login";
-});
-keyUser != null ? $("#sign-user").html(`<button class="btn btn-primary mt-3">Logout</button>`) : false;
-})
+  
+
